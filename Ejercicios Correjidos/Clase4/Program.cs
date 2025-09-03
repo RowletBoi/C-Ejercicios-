082aea18c110b0d8e133e66a2b9ec917c04c5276
RowletBoi <jdonaire@uamv.edu.ni>
2025-09-03 00:22:14 -0600

    Correcciones de Clase #4
    eso es

---
+++ Ejercicios Correjidos/Clase4/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Runtime.Serialization.Formatters;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        ////////////////////////Ejemplo 1//////////////////
+        int i = 0;
+        while (i < 5)
+        {
+            Console.WriteLine(i);
+            i++;
+        }
+        //El error era que el valor nunca se sumaba,
+        //asi que era infinito el ciclo
+        /////////////////////////Ejemplo 2//////////////////
+        int edad = 20;
+        if (edad > 18 && edad < 65)
+        {
+            Console.WriteLine("Edad valida para trabajar.");
+
+        }
+        //El error era que el operador logico era || (o)
+        //y debia ser && (y)
+        /////////////////////////Ejemplo 3//////////////////
+        for (int j = 0; j < 10; j++)
+        {
+            Console.WriteLine(j);
+        }
+        //El error era que no tenia punto y coma antes del j<10
+        /////////////////////////Ejemplo 4//////////////////
+        int numero = 10;
+        bool esMayor;
+        esMayor = (numero > 5) ? true : false;
+        Console.WriteLine("¿Es mayor que 5? " + esMayor);
+        //El error era que faltaba el operador true : false
+        /////////////////////////Ejemplo 5//////////////////
+        int resultado = 5;
+        resultado = resultado + 10;
+        Console.WriteLine(resultado);
+        //El error era que faltaba darle un valor a resultado  
+        //////////////////////////Ejemplo 6//////////////////
+        int edad2 = 17;
+        if (edad2 >= 18)
+        {
+            Console.WriteLine("Es mayor de edad.");
+            Console.WriteLine("Puede votar.");
+        }
+        else
+        {
+            Console.WriteLine("Es menor de edad.");
+        }
+        //El error era que faltaba el los {} en el else y if 
+
+        
+    }
+}
---
+++ Ejercicios Correjidos/EjercicioVender/Program.cs
@@ -0,0 +1,58 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Ferreteria");
+    double  precio, total = 0;
+    int productosVendidos = -1 , prod = 0;
+    Console.WriteLine("1.....Taladro..C$350.00");
+    Console.WriteLine("2.....Martillo...C$600.00");
+    Console.WriteLine("3.....Tubo PVC..C$150.00");
+    Console.WriteLine("4.....Llave Inglesa.C$500.00");
+    
+
+    do
+    {
+        Console.WriteLine("Ingrese el numero del producto que desea llevar: ");
+        Console.WriteLine("(Ingrese '0' para salir)");
+
+        prod = Convert.ToInt32(Console.ReadLine());
+            switch (prod)
+            {
+                case 1:
+                    precio = 350;
+                    total += precio;
+                    break;
+                case 2:
+                    precio = 600;
+                    total += precio;
+                    break;
+                case 3:
+                    precio = 150;
+                    total += precio;
+                    break;
+                case 4:
+                    precio = 500;
+                    total += precio;
+                    break;
+                case 0:
+                    Console.WriteLine("Calculando...");
+                    break;
+            default:
+                Console.WriteLine("Ingrese un numero entre 1 - 4");
+                
+                break;
+
+        }
+        productosVendidos++;
+    } while (prod != 0);
+
+
+    Console.WriteLine($"Productos vendidos: {productosVendidos}");
+    Console.WriteLine($"Total: ${total}");
+    if (total > 10000)
+    {
+        Console.WriteLine("Venta mayorista detectada.");
+    }
+    }
+}
